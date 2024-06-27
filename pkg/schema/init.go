9446ae07085c1fa975f17d4a77c7750ef5af611b
bartoszmajsak <bartosz.majsak@gmail.com>
2024-06-27 18:58:03 +0200

    chore(lint): configures importas for authorino

--- pkg/schema/init.go
+++ pkg/schema/init.go
@@ -1,7 +1,7 @@
 package schema
 
 import (
-	authorino "github.com/kuadrant/authorino/api/v1beta2"
+	authorinov1beta2 "github.com/kuadrant/authorino/api/v1beta2"
 	istiosecurityv1beta1 "istio.io/client-go/pkg/apis/security/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -13,6 +13,6 @@ import (
 func RegisterSchemes(s *runtime.Scheme) {
 	utilruntime.Must(metav1.AddMetaToScheme(s))
 	utilruntime.Must(clientgoscheme.AddToScheme(s))
-	utilruntime.Must(authorino.AddToScheme(s))
+	utilruntime.Must(authorinov1beta2.AddToScheme(s))
 	utilruntime.Must(istiosecurityv1beta1.AddToScheme(s))
 }
