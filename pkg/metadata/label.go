0149baa222b29d9e96ff94d31f23497cae8a48d6
bartoszmajsak <bartosz.majsak@gmail.com>
2024-08-06 18:22:40 +0200

    feat(metadata): adds owner-kind label
    This change introduces platform.opendatahub.io/owner-kind label next to
    already existing owner-name. Without specified kind when given controller watches multiple
    resources the clash of CR names might happen and by using owner-name as the only
    criteria we might modify or delete resources which are not related to a given
    CR. By adding platform.opendatahub.io/owner-kind we can narrow the query to kube-apiserver
    to only fetch relevant resource.

--- pkg/metadata/label.go
+++ pkg/metadata/label.go
@@ -3,12 +3,15 @@ package metadata
 import "github.com/opendatahub-io/odh-platform/version"
 
 var Labels = struct { //nolint:gochecknoglobals //reason: anonymous struct is used for grouping labels together instead of consts
-	AppPartOf       string
-	AppComponent    string
-	AppName         string
-	AppVersion      string
-	AppManagedBy    string
-	OwnerName       string
+	AppPartOf    string
+	AppComponent string
+	AppName      string
+	AppVersion   string
+	AppManagedBy string
+	// OwnerName is the name of the owner of the resource combined with its namespace separated by a dash if applicable.
+	OwnerName string
+	// OwnerKind is the kind of the owner of the resource.
+	OwnerKind       string
 	RoutingExported string
 }{
 	AppPartOf:       "app.kubernetes.io/part-of",
@@ -17,6 +20,7 @@ var Labels = struct { //nolint:gochecknoglobals //reason: anonymous struct is us
 	AppVersion:      "app.kubernetes.io/version",
 	AppManagedBy:    "app.kubernetes.io/managed-by",
 	OwnerName:       "platform.opendatahub.io/owner-name",
+	OwnerKind:       "platform.opendatahub.io/owner-kind",
 	RoutingExported: "routing.opendatahub.io/exported",
 }
 
--- pkg/metadata/meta.go
+++ pkg/metadata/meta.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // Options allows to add additional settings for the object being created through a chain
@@ -18,6 +19,25 @@ func ApplyMetaOptions(obj metav1.Object, opts ...Options) error {
 	}
 
 	return nil
+}
+
+// WithOwnerLabels sets the owner labels on the object based on source object.
+// Those labels can be used to find all related resources across the cluster
+// which are owned by the source object by leveraging label selectors.
+// This can be particularly useful for garbage collection when source object is namespace-scoped
+// and related resources are created in a different namespace or are cluster-scoped.
+func WithOwnerLabels(source client.Object) Options {
+	ownerName := source.GetName()
+	if source.GetNamespace() != "" {
+		ownerName = source.GetNamespace() + "-" + ownerName
+	}
+
+	ownerKind := source.GetObjectKind().GroupVersionKind().Kind
+
+	return WithLabels(
+		Labels.OwnerName, ownerName,
+		Labels.OwnerKind, ownerKind,
+	)
 }
 
 func WithLabels(labels ...string) Options {
